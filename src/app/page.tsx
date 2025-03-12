11cba1ee3203212bb0c2ccfd78d35c0d9aab10cf
HANSOL <121593205+sol322@users.noreply.github.com>
2025-03-12 20:37:28 +0900

    Feat(#5): 랜딩페이지 세션4 개발 (#13)
    * Feat: 랜딩페이지 세션4
    
    * Fix: 공백 section 제거
    
    * Feat: 버튼에도 나타나기 모션 적용

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,20 @@
+'use client';
+
 import Image from 'next/image';
 import Link from 'next/link';
+import { motion } from 'framer-motion';
+
+const ScrollInMotion = {
+  hidden: { opacity: 0, y: 70 },
+  visible: { opacity: 1, y: 0 },
+  transition: { duration: 1.5 },
+};
+
+const DampingMotion = {
+  whileHover: { scale: 1.1 },
+  whileTap: { scale: 0.9 },
+  transition: { type: 'spring', stiffness: 400, damping: 10 },
+};
 
 export default function Home() {
   return (
@@ -29,6 +44,32 @@ export default function Home() {
           </div>
         </div>
         <div className="h-[15px] w-full bg-[url('/assets/images/pattern.png')] bg-repeat drop-shadow-xl"></div>
+      </section>
+      <section>
+        <div className="h-[15px] w-full scale-y-[-1] bg-[url('/assets/images/pattern.png')] bg-repeat drop-shadow-xl"></div>
+        <div className="border-line-100 relative z-1 flex h-[672px] w-full items-center justify-center border-b-[1px] bg-blue-100 bg-[repeating-linear-gradient(0deg,#ffffff_0px,#ffffff_24px,#F2F2F2_24px,#F2F2F2_25px)]">
+          <div className="pc:gap-[48px] flex flex-col items-center justify-center gap-[32px]">
+            <motion.div initial="hidden" whileInView="visible" variants={ScrollInMotion} viewport={{ once: true }}>
+              <Image
+                className="pc:w-[182px] pc:h-[104px]"
+                src="/assets/images/slogan-text.svg"
+                width={122}
+                height={70}
+                alt="날마다 에피그램"
+              />
+            </motion.div>
+            <motion.div initial="hidden" whileInView="visible" variants={ScrollInMotion} viewport={{ once: true }}>
+              <motion.div {...DampingMotion}>
+                <Link
+                  href={'/epigrams'}
+                  className="pc:text-pre-xl pc:px-[108px] pc:py-[16px] bg-black-500 text-pre-lg rounded-[12px] px-[28px] py-[11px] font-semibold text-blue-100"
+                >
+                  시작하기
+                </Link>
+              </motion.div>
+            </motion.div>
+          </div>
+        </div>
       </section>
     </div>
   );
