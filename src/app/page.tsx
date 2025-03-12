8b5fb5805dce4c13571d36528a318e4616708dbc
김은경 <kng4316@gmail.com>
2025-03-12 20:50:15 +0900

    Feat: 랜딩페이지 세션1 반응형 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,25 +21,25 @@ export default function Home() {
     <div className="flex h-[1000px] flex-col bg-blue-200">
       <section>
         <div className="border-line-100 relative z-1 flex h-[672px] w-full items-center justify-center border-b-[1px] bg-blue-100 bg-[repeating-linear-gradient(0deg,#ffffff_0px,#ffffff_24px,#F2F2F2_24px,#F2F2F2_25px)]">
-          <div className="flex flex-col items-center justify-center gap-[24px]">
-            <div className="flex flex-col gap-[6px]">
-              <h1 className="font-iropke text-iro-2xl text-black-500 flex justify-center text-center font-normal">
+          <div className="tablet:gap-[32px] pc:gap-[48px] flex flex-col items-center justify-center gap-[24px]">
+            <div className="tablet:gap-[24px] pc:gap-[40px] flex flex-col gap-[6px]">
+              <h1 className="font-iropke text-iro-2xl text-black-500 tablet:text-iro-3xl pc:text-iro-4xl pc:leading-[64px] flex justify-center text-center font-normal">
                 나만 갖고 있기엔
                 <br /> 아까운 글이 있지 않나요?
               </h1>
-              <p className="text-black-300 font-iropke text-iro-md text-center font-normal">
+              <p className="text-black-300 font-iropke text-iro-md tablet:text-iro-xl text-center font-normal">
                 다른 사람들과 감정을 공유해 보세요.
               </p>
             </div>
             <Link
               href={'/epigrams'}
-              className="bg-black-500 text-pre-lg rounded-[12px] px-[28px] py-[11px] font-semibold text-blue-100"
+              className="bg-black-500 text-pre-lg pc:text-pre-xl pc:px-[108px] pc:py-[16px] rounded-[12px] px-[28px] py-[11px] font-semibold text-blue-100"
             >
               시작하기
             </Link>
           </div>
           <div className="absolute bottom-[20px] flex flex-col items-center justify-center gap-[4px]">
-            <p className="text-pre-xs font-semibold text-blue-400">더 알아보기</p>
+            <p className="text-pre-xs tablet:text-pre-lg font-semibold text-blue-400">더 알아보기</p>
             <Image src="/assets/icons/chevron.svg" alt="화살표" width={24} height={24} />
           </div>
         </div>
