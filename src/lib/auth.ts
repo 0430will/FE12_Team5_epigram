95521776086f9da44b75ee23bb71b6f47aa56c6d
김은경 <kng4316@gmail.com>
2025-03-18 00:35:28 +0900

    Feat: 공통 컴포넌트 오늘의 감정 UI와 Api 연결

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -49,3 +49,35 @@ export async function PostSignUp(formData: FormData) {
     throw new Error('알 수 없는 오류가 발생했습니다.');
   }
 }
+
+//오늘의 감정
+export async function PostTodayEmotion(emotionName: string) {
+  try {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/emotionLogs/today`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer 토큰`,
+      },
+      body: JSON.stringify({
+        emotion: emotionName,
+      }),
+    });
+
+    if (response.status === 401) {
+      throw new Error('로그인이 만료되었습니다.');
+    }
+
+    if (!response.ok || response === null) {
+      throw new Error('서버 오류가 발생하였습니다.');
+    }
+    const data = response.json();
+    return data;
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      console.error(`${error.message}`);
+    } else {
+      console.error('감정을 등록하는데 실패했습니다.');
+    }
+  }
+}
