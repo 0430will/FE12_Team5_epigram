d15afa536c9217d669929a76fc40fa55cd607443
HANSOL <121593205+sol322@users.noreply.github.com>
2025-03-18 15:28:11 +0900

    Feat: 샘플 페이지

---
+++ src/app/sample/_components/Sol.tsx
@@ -0,0 +1,17 @@
+export default function Sol() {
+  return (
+    <>
+      <div className="border border-gray-200 bg-gray-50 p-5">
+        <h3 className="mb-5 text-xl font-bold">1. Modal</h3>
+        <p className="mb-5 leading-7">
+          props
+          <br />
+          1️⃣
+        </p>
+        <div className="bg-white p-5">
+          <button className="cursor-pointer rounded-md bg-pink-500 px-5 py-2 font-bold text-white">모달 열기</button>
+        </div>
+      </div>
+    </>
+  );
+}
