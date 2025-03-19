0d06e2537d4459dcd835ba94461bb09fd02534ce
김은경 <98223616+eunkyung01@users.noreply.github.com>
2025-03-19 10:40:42 +0900

    Feat(#27): 공통컴포넌트 오늘의감정 (#38)
    * Feat: 공통 컴포넌트 오늘의 감정 UI와 Api 연결
    
    * Feat: main/mypage 종류 구분 prop 추가
    
    * Chore: 테스트 페이지에 컴포넌트 추가
    
    * Chore: api 파일 위치 변경

---
+++ src/components/TodayEmotion.tsx
@@ -0,0 +1,99 @@
+'use client';
+
+import { PostTodayEmotion } from '@/lib/Emotionlog';
+import Image from 'next/image';
+import { useState } from 'react';
+
+const EmotionData = {
+  감동: {
+    image: '/assets/images/heartFace.png',
+    name: 'MOVED',
+  },
+  기쁨: {
+    image: '/assets/images/smiling.png',
+    name: 'HAPPY',
+  },
+  고민: {
+    image: '/assets/images/thinking.png',
+    name: 'WORRIED',
+  },
+  슬픔: {
+    image: '/assets/images/sad.png',
+    name: 'SAD',
+  },
+  분노: {
+    image: '/assets/images/angry.png',
+    name: 'ANGRY',
+  },
+};
+
+type EmotionKey = keyof typeof EmotionData;
+
+interface TodayEmotionProps {
+  emotionType: 'main' | 'mypage';
+}
+
+interface EmotionProps {
+  emotion: EmotionKey;
+  isSelected: boolean;
+  onClick: () => void;
+  emotionType: string;
+}
+
+function Emotion({ emotion, isSelected, onClick, emotionType }: EmotionProps) {
+  return (
+    <div className="flex cursor-pointer flex-col items-center gap-[8px]" onClick={onClick}>
+      <div
+        className={`tablet:h-[64px] tablet:w-[64px] pc:w-[96px] pc:h-[96px] flex h-[56px] w-[56px] items-center justify-center rounded-[16px] ${isSelected ? `relative` : `bg-[#AFBACD]/15`}`}
+      >
+        <div className="pc:h-[48px] pc:w-[48px] relative h-[32px] w-[32px] object-contain">
+          <Image
+            priority
+            src={EmotionData[emotion].image}
+            fill
+            alt={`${emotion} 아이콘`}
+            sizes="(max-width: 767px) 32px, 48px"
+          />
+        </div>
+        {isSelected && (
+          <div
+            className={`${emotionType === 'main' ? 'border-yellow' : 'border-green'} pc:border-[4px] absolute top-0 right-0 bottom-0 left-0 rounded-[16px] border-[3px]`}
+          ></div>
+        )}
+      </div>
+      <span
+        className={`text-pre-xs tablet:text-pre-md pc:text-pre-xl font-semibold ${isSelected ? `text-sub-blue-1` : `text-gray-300`}`}
+      >
+        {emotion}
+      </span>
+    </div>
+  );
+}
+
+export default function TodayEmotion({ emotionType }: TodayEmotionProps) {
+  const [selectedEmotion, setSelectedEmotion] = useState<EmotionKey | null>(null);
+
+  const OnClickEmotion = async (emotion: EmotionKey) => {
+    if (selectedEmotion === emotion) return;
+
+    const response = await PostTodayEmotion(EmotionData[emotion].name);
+    if (!response) return;
+
+    console.log(`오늘의 감정 등록 완료: ${emotion}`);
+    setSelectedEmotion(emotion);
+  };
+
+  return (
+    <div className="pc:gap-[16px] flex justify-center gap-[8px]">
+      {Object.keys(EmotionData).map((emotion) => (
+        <Emotion
+          key={emotion}
+          emotion={emotion as EmotionKey}
+          emotionType={emotionType}
+          isSelected={selectedEmotion === emotion}
+          onClick={() => OnClickEmotion(emotion as EmotionKey)}
+        />
+      ))}
+    </div>
+  );
+}
