5dc5b9326475dd995f8a4c73744cfe4a7c0612c0
HANSOL <121593205+sol322@users.noreply.github.com>
2025-03-24 16:29:52 +0900

    Feat: 주석 수정

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -5,8 +5,8 @@ export default function MyPage() {
     <main>
       <div className="pc:mt-[120px] shadow-[0px 0px 36px 0px rgba(0, 0, 0, 0.05)] mt-[64px] rounded-3xl bg-white">
         <div className="relative top-[-53px] m-auto max-w-[680px] px-[24px]">
-          {/* TODO: 프로필 컴포넌트, 오늘의 감정 컴포넌트 구현 */}
-          {/* ✅ 프로필 <section> 코드는 예시 입니다. 지우고 작업해주세요:-) */}
+          {/* ✅ 여기에 프로필 컴포넌트과 오늘의 감정 컴포넌트를 구현해주세요. */}
+          {/* ✅ <section> 코드는 예시 입니다. 지우고 작업해주세요:-) */}
           <section className="flex flex-col items-center">
             <div className="mb-[16px] h-[120px] w-[120px] rounded-full bg-amber-300"></div>
             <span>프로필</span>
