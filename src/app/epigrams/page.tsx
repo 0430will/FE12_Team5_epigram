fd8d60a92886129aa20f7188d61c89b7c40ef698
서경수 <kssuh2956@gmail.com>
2025-03-25 16:01:00 +0900

    Fix : 배포시 redirect uri 환경변수 설정

--- src/app/epigrams/page.tsx
+++ src/app/epigrams/page.tsx
@@ -4,7 +4,6 @@ import TodayEmotion from '@/components/TodayEmotion';
 export default function Page() {
   return (
     <>
-      <MainHeader />
       <div>메인페이지</div>
       <TodayEmotion emotionType="main" />
     </>
