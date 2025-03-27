c0c37752b919c9b5e6bc26bbfd00033fc1cf0f7f
rnquf59 <81212279+rnquf59@users.noreply.github.com>
2025-03-28 04:04:41 +0900

    feat: 마이페이지 최신댓글 / 상세페이지 - 댓글 컴포넌트 구현 (#92)

---
+++ src/app/feed/[id]/_component/CommentCount.tsx
@@ -0,0 +1,12 @@
+interface CommentCountProps {
+  label?: string; // 기본값: "댓글"
+  count: number;
+}
+
+export default function CommentCount({ label = '댓글', count }: CommentCountProps) {
+  return (
+    <div className="tablet:mb-6 pc:mb-6 tablet:text-base pc:text-lg mb-4 text-sm font-semibold text-gray-800">
+      {label} <span className="text-black">({count})</span>
+    </div>
+  );
+}
