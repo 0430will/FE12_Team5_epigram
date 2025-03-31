81eee26840d67dd3663b9ec5c70dd50e324881c9
서경수 <kssuh2956@gmail.com>
2025-03-31 14:29:21 +0900

    commit

--- src/app/sample/_components/Su.tsx
+++ src/app/sample/_components/Su.tsx
@@ -1,10 +1,7 @@
-import SignOut from '@/app/_components/SignOutButton';
-
 export default function Su() {
   return (
     <>
       <div className="border border-gray-200 bg-gray-50 p-5">
-        <SignOut />
         <p className="mb-5 leading-7">설명</p>
         <div className="bg-white p-5">컴포넌트 구현</div>
       </div>
