00bb21bbe613246fa8f793632afcca9ffdc33184
김은경 <kng4316@gmail.com>
2025-03-31 14:13:15 +0900

    Fix: 세션의 유저 토큰 연결

--- src/app/mypage/_components/MyCalender/MyCalender.tsx
+++ src/app/mypage/_components/MyCalender/MyCalender.tsx
@@ -5,12 +5,16 @@ import CustomCalender from './CustomCalender';
 import { useEffect, useState } from 'react';
 import { GetMonthEmotion } from '@/lib/Emotionlog';
 import moment from 'moment';
+import { useSession } from 'next-auth/react';
 
 export default function MyCalender() {
   const [data, setData] = useState<EmotionLog[]>();
   const [displayMonth, setDisplayMonth] = useState<Date>(new Date());
+  const { data: session } = useSession();
+  const writerId = session?.user.id ? Number(session.user.id) : undefined;
 
   const getData = async () => {
+    if (!writerId) return;
     const response = await GetMonthEmotion(1349, moment(displayMonth).year(), moment(displayMonth).month() + 1);
     if (!response) return;
 
