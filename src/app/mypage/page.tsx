327fd82f16db4ebf0666d2a0151fe4223f6358f9
김은경 <98223616+eunkyung01@users.noreply.github.com>
2025-03-31 10:22:32 +0900

    Feat(#94): 마이페이지 캘린더 (#108)
    * Feat: 캘린더 유저log 가져오기
    
    * Feat: 필터 상태 관리
    
    * Feat: 필터링과 데이터 연결
    
    * Chore: 반응형 디자인
    
    * Chore: 중복 코드 제거
    
    * Chore: 충돌 해결

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -1,5 +1,6 @@
 import EmotionPieChart from './_components/EmotionPieChart';
 import EmotionRankList from './_components/EmotionRankList';
+import MyCalender from './_components/MyCalender/MyCalender';
 import MyTabMenu from './_components/MyTabMenu';
 import TodayEmotion from '@/components/TodayEmotion';
 import MyUserProfile from './_components/MyUserProfile';
@@ -10,9 +11,10 @@ export default function MyPage() {
       <div className="pc:mt-[120px] shadow-[0px 0px 36px 0px rgba(0, 0, 0, 0.05)] mt-[64px] rounded-3xl bg-white">
         <div className="relative top-[-53px] m-auto max-w-[680px] px-[24px]">
           <MyUserProfile />
+          <MyCalender />
         </div>
         <TodayEmotion emotionType="mypage" />
-        <div className="pc:pb-[82px] tablet:pb-[56px] pc:gap-[48px] m-auto flex h-full w-full max-w-[680px] flex-col justify-center gap-[16px] px-[24px] pt-[58px] pb-[40px]">
+        <div className="pc:pb-[82px] tablet:pb-[56px] pc:gap-[48px] m-auto flex h-full w-full max-w-[680px] flex-col justify-center gap-[16px] px-[24px] pb-[40px] pt-[58px]">
           <h1 className="pc:text-pre-2xl text-pre-lg font-weight-semibold">감정 차트</h1>
           <div className="border-line-100 pc:h-[264px] flex h-full w-full items-center justify-between rounded-lg border p-[30px]">
             <EmotionPieChart />
@@ -20,7 +22,7 @@ export default function MyPage() {
           </div>
         </div>
       </div>
-      <div className="pc:pt-[103px] m-auto max-w-[680px] px-[24px] pt-[56px] pb-[114px]">
+      <div className="pc:pt-[103px] m-auto max-w-[680px] px-[24px] pb-[114px] pt-[56px]">
         <MyTabMenu />
       </div>
     </main>
