a1c53897e0b381a1a2286f286416480ae206daff
10gosu1 <kssuh2956@gmail.com>
2025-04-02 10:28:27 +0900

    Fix(#117) : 마이페이지 오늘의 감정 컴포넌트의 헤더 (#130)
    * commit
    
    * fix vercel error
    
    * Feat: 마이페이지 로그아웃 버튼 구현
    
    * Fix: 마이페이지 오늘의 감정 컴포넌트 제목, 감정 변경시 createdAt 표시
    
    * Fix: user.ts의 GetUserInfo에서 데이터를 불러올 시 console.log 코드 제거
    
    ---------
    
    Co-authored-by: yujuseop <dbwntjq@naver.com>

--- src/app/sample/_components/Su.tsx
+++ src/app/sample/_components/Su.tsx
@@ -1,8 +1,18 @@
+
+'use client';
+
+import { signOut } from 'next-auth/react';
 export default function Su() {
   return (
     <>
       <div className="border border-gray-200 bg-gray-50 p-5">
         <p className="mb-5 leading-7">설명</p>
+        <div
+          className="text-pre-md font-weight-regular cursor-pointer"
+          onClick={() => signOut()} //로그아웃
+        >
+          로그아웃
+        </div>
         <div className="bg-white p-5">컴포넌트 구현</div>
       </div>
     </>
