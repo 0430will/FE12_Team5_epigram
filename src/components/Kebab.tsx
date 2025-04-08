67ee07acd9737f6832b8141e249f685d6f47570f
유주섭 <noname@nonameui-MacBookAir.local>
2025-04-08 15:50:55 +0900

    fix:로그인과 회원가입 성공 및 실패 시 toast적용 그리고 메인헤더 z-index적용

--- src/components/Kebab.tsx
+++ src/components/Kebab.tsx
@@ -9,7 +9,7 @@ interface KebabProps {
 
 export default function Kebab({ label1, label2, onCLick1, onClick2 }: KebabProps) {
   return (
-    <div className="bg-bg-100 pc:h-[112px] pc:w-[134px] pc:top-[44px] absolute top-[31px] right-0 flex h-[80px] w-[97px] flex-col overflow-hidden rounded-[16px] border border-blue-300">
+    <div className="bg-bg-100 pc:h-[112px] pc:w-[134px] pc:top-[44px] absolute top-[31px] right-0 z-1 flex h-[80px] w-[97px] flex-col overflow-hidden rounded-[16px] border border-blue-300">
       <div
         className="hover:bg-sub-gray-2 text-pre-md font-regular text-black-600 pc:text-pre-xl flex flex-1 cursor-pointer items-center justify-center"
         onClick={() => onCLick1()}
