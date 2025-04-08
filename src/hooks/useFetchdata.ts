cd52acdd47b31c4b6035d6dd2e456f64d4ad025f
JEONBYEONGJIN <qudwls5020@naver.com>
2025-04-08 20:37:19 +0900

    feat: 닉네임 수정시 헤더 연동

--- src/hooks/useFetchdata.ts
+++ src/hooks/useFetchdata.ts
@@ -1,45 +1,30 @@
+'use client';
+
+import useSWR from 'swr';
 import { useSession } from 'next-auth/react';
-import { useEffect, useState } from 'react';
-import { User } from '@/types/User';
 import { GetUserInfo } from '@/lib/User';
+
+const fetcher = async (userId: number) => {
+  return await GetUserInfo(userId);
+};
 
 export default function useFetchUser() {
   //user의 정보를 가져오는 hook입니다. 필요한곳에 useFetchUser를 불러서 쓰면 됩니다.
   //활용 예시는 MyUserProfile.tsx를 확인해주세요
-  const { data: session, status } = useSession();
+  const { data: session } = useSession();
   const userId = Number(session?.user?.id);
-  const [userData, setUserData] = useState<{
-    isLoading: boolean;
-    user: User | null;
-  }>({
-    isLoading: true,
-    user: null,
-  });
 
-  useEffect(() => {
-    if (status === 'loading') return;
+  const {
+    data: user,
+    error,
+    isLoading,
+    mutate: refetchUser,
+  } = useSWR(userId ? `/user/${userId}` : null, () => fetcher(userId));
 
-    const fetchUser = async () => {
-      try {
-        const data = await GetUserInfo(userId);
-
-        if (data) {
-          setUserData({
-            isLoading: false,
-            user: data,
-          });
-        }
-      } catch (error) {
-        console.error('Error fetching user data:', error);
-        setUserData({
-          isLoading: false,
-          user: null,
-        });
-      }
-    };
-
-    fetchUser();
-  }, [status, session]);
-
-  return { ...userData, setUser: (user: User) => setUserData({ isLoading: false, user }) };
+  return {
+    user,
+    isLoading,
+    error,
+    refetchUser,
+  };
 }
