eacda4748e4ee00b36f1fbb04cbac23d6ab1a1f3
HANSOL <121593205+sol322@users.noreply.github.com>
2025-04-10 16:40:35 +0900

    Feat: 스켈레톤 UI 간격 조정

--- src/components/skeletons/SkeletonMyCart.tsx
+++ src/components/skeletons/SkeletonMyCart.tsx
@@ -4,7 +4,7 @@ export default function SkeletonMyCart() {
       <div className="flex w-full flex-col items-center">
         <div className="pc:w-[170px] pc:h-[170px] h-[110px] w-[110px] rounded-full bg-blue-300"></div>
       </div>
-      <div className="flex h-full w-full flex-col justify-center gap-[8px]">
+      <div className="pc:gap-[13px] flex h-full w-full flex-col justify-center gap-[8px]">
         {Array.from({ length: 5 }).map((_, index) => (
           <div key={index} className="flex items-center justify-center gap-2">
             <div className="pc:w-[24px] pc:h-[24px] h-[16px] w-[16px] rounded-full bg-blue-300"></div>
